d29ce19991544716c85f3dbada74c948a5d594b8
Sergey Khmelevskoy <xs290@me.com>
2021-02-02 01:09:00 +0300

    sidebar second level

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -23,9 +23,43 @@
     clickListeners: function() {
       let _this = this;
 
-      _document.on('mousenter', '.js-sidebar-nav a', function() {
-        let $link = _this.data.menu.addClass('.is-active');
-      });
+      // show menu when hovered any nav's
+      // add hovered class
+      // light up menu by index
+      _document
+        .on('mouseenter', '.js-sidebar-nav a', function() {
+          let $link = $(this);
+          let $menu = _this.data.menu;
+          let dataIndex = $link.parent().data('index');
+          // console.log({ dataIndex: dataIndex });
+
+          $link.addClass('is-active');
+          $menu.addClass('is-active');
+
+          // light up menu by index
+          $menu.find('li').removeClass('is-active');
+          $menu.find('li[data-index="' + dataIndex + '"]').addClass('is-active');
+        })
+
+        .on('mouseenter', '.js-sidebar-menu a', function() {
+          let $link = $(this);
+          let $nav = _this.data.nav;
+          let $menu = _this.data.menu;
+          let dataIndex = $link.parent().data('index');
+
+          $link.addClass('is-active');
+
+          // light up menu by index (reverse)
+          $menu.find('li').removeClass('is-active');
+          $nav.find('li').removeClass('is-active');
+          $nav.find('li[data-index="' + dataIndex + '"]').addClass('is-active');
+        })
+
+        .on('mouseleave', '.sidebar-global-container', function() {
+          _this.data.menu.removeClass('is-active');
+          _this.data.nav.find('li').removeClass('is-active');
+          _this.data.nav.find('li').removeClass('is-active');
+        });
     },
     // listenScroll: function() {
     //   _window.on('scroll', debounce(this.scrollHeaderDebouce.bind(this), 1250, { trailing: true }));
