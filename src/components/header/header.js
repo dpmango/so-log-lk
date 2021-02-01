1192ea267aea9756e10b705bae4990fc799e22e7
Sergey Khmelevskoy <xs290@me.com>
2021-02-02 01:42:08 +0300

    menu finalized

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -16,15 +16,11 @@
     },
     init: function(fromPjax) {
       if (!fromPjax) {
-        this.getHeaderParams();
-        this.hamburgerClickListener();
-        this.listenScroll();
-        this.listenResize();
+        // this.getHeaderParams();
+        // this.hamburgerClickListener();
+        // this.listenScroll();
+        // this.listenResize();
       }
-
-      this.closeMobileMenu();
-      this.setMenuClass();
-      this.controlHeaderClass();
     },
     getHeaderParams: function() {
       let $header = $('.header');
@@ -36,31 +32,25 @@
         bottomPoint: headerHeight,
       };
     },
-    closeMobileMenu: function() {
-      $('[js-hamburger]').removeClass('is-active');
-      $('.mobile-navi').removeClass('is-active');
+    // hamburgerClickListener: function() {
+    //   _document.on('click', '[js-hamburger]', function() {
+    //     $(this).toggleClass('is-active');
+    //     $('.mobile-navi').toggleClass('is-active');
 
-      APP.Plugins.ScrollBlock.enableScroll();
-    },
-    hamburgerClickListener: function() {
-      _document.on('click', '[js-hamburger]', function() {
-        $(this).toggleClass('is-active');
-        $('.mobile-navi').toggleClass('is-active');
-
-        if ($(this).is('.is-active')) {
-          APP.Plugins.ScrollBlock.disableScroll();
-        } else {
-          APP.Plugins.ScrollBlock.enableScroll();
-        }
-      });
-    },
-    listenScroll: function() {
-      _window.on('scroll', this.scrollHeader.bind(this));
-      _window.on('scroll', debounce(this.scrollHeaderDebouce.bind(this), 1250, { trailing: true }));
-    },
-    listenResize: function() {
-      _window.on('resize', debounce(this.getHeaderParams.bind(this), 100));
-    },
+    //     if ($(this).is('.is-active')) {
+    //       APP.Plugins.ScrollBlock.disableScroll();
+    //     } else {
+    //       APP.Plugins.ScrollBlock.enableScroll();
+    //     }
+    //   });
+    // },
+    // listenScroll: function() {
+    //   _window.on('scroll', this.scrollHeader.bind(this));
+    //   _window.on('scroll', debounce(this.scrollHeaderDebouce.bind(this), 1250, { trailing: true }));
+    // },
+    // listenResize: function() {
+    //   _window.on('resize', debounce(this.getHeaderParams.bind(this), 100));
+    // },
     makeHeaderVisible: function() {
       this.data.header.container.addClass(this.data.classes.visibleClass);
       $('body').addClass(this.data.classes.bodyFixedVisible);
@@ -70,72 +60,6 @@
       this.data.header.container.removeClass(this.data.classes.visibleClass);
       $('body').removeClass(this.data.classes.bodyFixedVisible);
       this.data.header.isFixedVisible = false;
-    },
-    scrollHeaderDebouce: function() {
-      // always show header after user stop scrolling
-      if (this.data.header.container !== undefined) {
-        this.makeHeaderVisible();
-      }
-    },
-    scrollHeader: function() {
-      if (this.data.header.container !== undefined) {
-        let fixedClass = 'is-fixed';
-        let visibleClass = 'is-fixed-visible';
-
-        // get scroll params from blocker function
-        let scroll = APP.Plugins.ScrollBlock.getData();
-
-        if (scroll.blocked) return;
-
-        if (scroll.y > this.data.header.bottomPoint) {
-          this.data.header.container.addClass(fixedClass);
-
-          if (scroll.y > this.data.header.bottomPoint * 2 && scroll.direction === 'up') {
-            this.makeHeaderVisible();
-          } else {
-            this.makeHeaderHidden();
-          }
-        } else {
-          // emulate position absolute by giving negative transform on initial scroll
-          let normalized = Math.floor(normalize(scroll.y, this.data.header.bottomPoint, 0, 0, 100));
-          let reverseNormalized = (100 - normalized) * -1;
-          reverseNormalized = reverseNormalized * 1.2; // a bit faster transition
-
-          this.data.header.container.css({
-            transform: 'translate3d(0,' + reverseNormalized + '%,0)',
-          });
-
-          this.data.header.container.removeClass(fixedClass);
-        }
-      }
-    },
-    setMenuClass: function() {
-      // SET ACTIVE CLASS IN HEADER
-      let headerMenuList = $('.header__menu li');
-      if (headerMenuList.length === 0) return;
-
-      headerMenuList.each(function(i, val) {
-        if (
-          $(val)
-            .find('a')
-            .attr('href') === window.location.pathname.split('/').pop()
-        ) {
-          $(val).addClass('is-active');
-        } else {
-          $(val).removeClass('is-active');
-        }
-      });
-    },
-    controlHeaderClass: function() {
-      this.data.header.container.attr('data-modifier', false);
-
-      let $modifierElement = $('.page')
-        .last()
-        .find('[js-header-class]');
-
-      if ($modifierElement.length > 0) {
-        this.data.header.container.attr('data-modifier', $modifierElement.data('class'));
-      }
     },
   };
 })(jQuery, window.APP);
--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -4,19 +4,23 @@
 (function($, APP) {
   APP.Components.Sidebar = {
     data: {
+      container: undefined,
       nav: undefined,
       menu: undefined,
     },
     init: function(fromPjax) {
       if (!fromPjax) {
         this.onMount();
+        // this.setPositions();
         this.clickListeners();
       }
     },
     onMount: function() {
+      let $container = $('.sidebar-global-container');
       let $nav = $('.js-sidebar-nav');
       let $menu = $('.js-sidebar-menu');
 
+      this.data.container = $container;
       this.data.nav = $nav;
       this.data.menu = $menu;
     },
@@ -26,15 +30,19 @@
       // show menu when hovered any nav's
       // add hovered class
       // light up menu by index
+
+      //TODO - add debounced function for mopuseout
       _document
         .on('mouseenter', '.js-sidebar-nav a', function() {
           let $link = $(this);
           let $menu = _this.data.menu;
+          let $container = _this.data.container;
           let dataIndex = $link.parent().data('index');
           // console.log({ dataIndex: dataIndex });
 
           $link.addClass('is-active');
           $menu.addClass('is-active');
+          $container.addClass('is-active');
 
           // light up menu by index
           $menu.find('li').removeClass('is-active');
@@ -57,10 +65,24 @@
 
         .on('mouseleave', '.sidebar-global-container', function() {
           _this.data.menu.removeClass('is-active');
+          _this.data.container.removeClass('is-active');
           _this.data.nav.find('li').removeClass('is-active');
           _this.data.nav.find('li').removeClass('is-active');
         });
     },
+    // setPositions: function() {
+    //   // position for 3rd level menu (__lvl-down)
+    //   // let $menu = this.data.menu;
+    //   // if ($menu.legth === 0) return;
+    //   // let $list = $menu.find('.s-menu__lvl-down');
+    //   // if ($list.length === 0) return;
+    //   // $list.each((i, ul) => {
+    //   //   let $ul = $(ul);
+    //   //   let $parentLi = $ul.parent('li');
+    //   //   let positionTop = $parentLi.offset().top;
+    //   //   $ul.css({ paddingTop: positionTop });
+    //   // });
+    // },
     // listenScroll: function() {
     //   _window.on('scroll', debounce(this.scrollHeaderDebouce.bind(this), 1250, { trailing: true }));
     // },
