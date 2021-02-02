b8e41679caf97b4fa0b43f5c15115b51d424fee4
Sergey Khmelevskoy <xs290@me.com>
2021-02-02 15:29:03 +0300

    new tables

--- src/js/modules/ui/tabs.js
+++ src/js/modules/ui/tabs.js
@@ -1,28 +1,47 @@
 //////////
 // Tabs
 //////////
-(function($, APP) {
+(function ($, APP) {
   APP.Plugins.Tabs = {
-    init: function(fromPjax) {
+    init: function (fromPjax) {
       if (!fromPjax) {
         this.clickListeners();
       }
+      this.checkHash();
     },
-    clickListeners: function() {
-      _document.on('click', '.js-tabs-nav a', function() {
+    changeActiveTab: function (dataHref) {
+      let $link = $('.js-tabs-nav a[href="#' + dataHref + '"]');
+      if ($link.length === 0) return;
+      let $container = $link.closest('.js-tabs');
+      let $li = $link.closest('li');
+
+      // li active
+      $li.addClass('is-active');
+      $li.siblings().removeClass('is-active');
+
+      // change active tab
+      let $tab = $container.find('.js-tab[data-tab="' + dataHref + '"]');
+      let $tabs = $container.find('.js-tab:not([data-tab="' + dataHref + '"])');
+
+      $tabs.hide();
+      $tab.fadeIn(250, function () {
+        $tab.addClass('is-active');
+      });
+    },
+    clickListeners: function () {
+      var _this = this;
+      _document.on('click', '.js-tabs-nav a', function () {
         let $link = $(this);
-        let $container = $link.closest('.js-tabs');
-        let $li = $(this).closest('li');
         let dataHref = $link.attr('href').slice(1, $link.attr('href').length);
 
-        let $tab = $container.find('.js-tab[data-tab="' + dataHref + '"]');
-        let $tabs = $container.find('.js-tab:not([data-tab="' + dataHref + '"])');
-
-        $tabs.hide();
-        $tab.fadeIn(250, function() {
-          $tab.addClass('is-active');
-        });
+        _this.changeActiveTab(dataHref);
       });
+    },
+    checkHash: function () {
+      let hash = window.location.hash;
+      if (hash.length === 0) return;
+
+      this.changeActiveTab(hash.slice(1, hash.length));
     },
   };
 })(jQuery, window.APP);
