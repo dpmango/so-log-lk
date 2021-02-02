4e292dfe3dbe83a8048163c4f7eb101676eff029
Sergey Khmelevskoy <xs290@me.com>
2021-02-02 17:37:43 +0300

    modal - upload

---
+++ src/js/modules/plugins/micromodal.js
@@ -0,0 +1,37 @@
+//////////
+// Micromodal
+//////////
+(function ($, APP) {
+  APP.Plugins.MicroModal = {
+    init: function (fromPjax) {
+      if (!fromPjax) {
+        this.clickListeners();
+      }
+      // https://micromodal.now.sh
+      MicroModal.init({
+        onShow: (modal) => {
+          APP.Plugins.ScrollBlock.disableScroll();
+        },
+        onClose: (modal) => {
+          APP.Plugins.ScrollBlock.enableScroll();
+        },
+        openClass: 'is-open',
+        disableScroll: false,
+        disableFocus: false,
+        awaitOpenAnimation: false,
+        awaitCloseAnimation: false,
+        debugMode: true,
+      });
+    },
+    clickListeners: function () {
+      // _document.on('click', '[data-custom-open]', function () {
+      //   let modalName = $(this).data('custom-open');
+      //   MicroModal.show(modalName);
+      // });
+      _document.on('click', '[data-micromodal-close]', function () {
+        let modalName = $(this).closest('.modal').attr('id');
+        MicroModal.close(modalName);
+      });
+    },
+  };
+})(jQuery, window.APP);
