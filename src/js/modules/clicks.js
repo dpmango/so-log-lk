17a0aee0f30aefd6938dc555b51301b3ff593d0a
Sergey Khmelevskoy <xs290@me.com>
2021-02-03 17:27:10 +0300

    auth responsive

--- src/js/modules/clicks.js
+++ src/js/modules/clicks.js
@@ -13,32 +13,8 @@
           if (dataHref && dataHref !== '#') {
             e.preventDefault();
             e.stopPropagation();
-            Barba.Pjax.goTo(dataHref);
+            window.location.href = dataHref;
           }
-        })
-        // prevent going the same link (if barba is connected)
-        .on('click', 'a, [js-link]', function (e) {
-          let href = $(this).data('href') || $(this).attr('href');
-          let path = window.location.pathname;
-
-          if (href === path.slice(1, path.length)) {
-            e.preventDefault();
-            e.stopPropagation();
-          }
-        })
-        // scroll to section
-        .on('click', 'a[href^="#section"]', function () {
-          // section scroll
-          let el = $(this).attr('href');
-          let topTarget = $(el).offset().top;
-
-          // $('body, html').animate({scrollTop: topTarget}, 1000);
-          TweenLite.to(window, 1, {
-            scrollTo: { y: topTarget, autoKill: false },
-            ease: easingSwing,
-          });
-
-          return false;
         })
         // grid toggler
         .on('click', '[js-show-grid]', function () {
